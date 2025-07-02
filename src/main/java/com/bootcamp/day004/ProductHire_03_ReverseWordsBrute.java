52d0a941069d34b4fe4211d26c513fafa9caeb72
Sanket Nakhate <sanketnakhate26@gmail.com>
2025-07-02 22:09:05 +0530

    added day 04

--- src/main/java/com/bootcamp/day004/ProductHire_03_ReverseWordsBrute.java
+++ src/main/java/com/bootcamp/day004/ProductHire_03_ReverseWordsBrute.java
@@ -3,14 +3,23 @@ package com.bootcamp.day004;
 public class ProductHire_03_ReverseWordsBrute {
     public static void main(String[] args) {
         String s = "  hello world  ";
-        String[] words = s.trim().split("\\s+");
-        StringBuilder sb = new StringBuilder();
+        StringBuilder result = new StringBuilder();
+        int end = s.length() - 1;
 
-        for (int i = words.length - 1; i >= 0; i--) {
-            sb.append(words[i]);
-            if (i != 0) sb.append(" ");
+        while (end >= 0) {
+            // Skip spaces
+            while (end >= 0 && s.charAt(end) == ' ') end--;
+            if (end < 0) break;
+
+            int start = end;
+            // Move start pointer to the beginning of the word
+            while (start >= 0 && s.charAt(start) != ' ') start--;
+
+            // Append the word to result
+            result.append(s.substring(start + 1, end + 1)).append(" ");
+            end = start - 1;
         }
 
-        System.out.println("Reversed: " + sb);
+        System.out.println("Reversed: " + result.toString().trim());
     }
 }
